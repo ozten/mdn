161b3d6bddb8b9ff281d944146b99cde63ccb7e6
Fred Wenzel <fwenzel@mozilla.com>
2010-06-21 17:33:40 +0000

    default manage and settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,20 @@
-# Django settings for mdn project.
+# Django settings for the reporter project.
 
-DEBUG = True
+import os
+import logging
+
+# Make filepaths relative to settings.
+ROOT = os.path.dirname(os.path.abspath(__file__))
+path = lambda *a: os.path.join(ROOT, *a)
+
+ROOT_PACKAGE = os.path.basename(ROOT)
+
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+LOG_LEVEL = logging.DEBUG
+SYSLOG_TAG = "http_app_reporter"
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
@@ -17,8 +30,16 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {'init_command': 'SET storage_engine=InnoDB',
+                    'charset' : 'utf8',
+                    'use_unicode' : True,
+                   },
     }
 }
+
+# Caching
+#CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
+CACHE_DEFAULT_PERIOD = 60 * 5  # 5 minutes
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -27,7 +48,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -45,20 +66,26 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = path('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '2y6$4oo&+)xem8_os$i&&)w-6za_461*z*0_po147xg0@04kzv'
+
+# Templates
+
+TEMPLATE_DIRS = (
+    path('templates'),
+)
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -67,21 +94,23 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+def JINJA_CONFIG():
+    import jinja2
+    config = {'extensions': ['jinja2.ext.with_', 'jinja2.ext.loopcontrols',
+                             'jinja2.ext.i18n'],
+              'finalize': lambda x: x if x is not None else ''}
+    return config
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'mdn.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -89,6 +118,5 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
 )
