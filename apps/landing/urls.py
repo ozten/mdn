7e1082ac72948f32c1d729ab52e80297e9788520
Fred Wenzel <fwenzel@mozilla.com>
2010-06-23 22:45:34 +0200

    mobile landing page

--- apps/landing/urls.py
+++ apps/landing/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('landing.views',
     url(r'^$', 'home', name='home'),
+    url(r'^mobile/?$', 'mobile', name='mobile'),
     url(r'^web/?$', 'web', name='web'),
 )
--- apps/landing/views.py
+++ apps/landing/views.py
@@ -6,6 +6,11 @@ def home(request):
     return jingo.render(request, 'landing/home.html')
 
 
+def mobile(request):
+    """Mobile landing page."""
+    return jingo.render(request, 'landing/mobile.html')
+
+
 def web(request):
     """Web landing page."""
     return jingo.render(request, 'landing/web.html')
