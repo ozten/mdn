2d0775234d0553386d68ca39793e051086dd822e
Fred Wenzel <fwenzel@mozilla.com>
2010-06-28 16:12:53 +0200

    added an update script for staging.

---
+++ bin/update_staging.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update script for staging server. Takes care of updating code repo, vendor
+# dir, and running DB migrations.
+
+HERE=`dirname $0`
+GIT=`which git`
+PYTHON=`which python2.6`
+
+pushd "$HERE/../" > /dev/null
+
+# pull actual code
+$GIT pull origin master
+
+# pull vendor repo
+pushd vendor > /dev/null
+$GIT pull origin master
+$GIT submodule update --init
+popd > /dev/null
+
+# Run database migrations.
+vendor/src/schematic/schematic migrations/
+
+popd > /dev/null
