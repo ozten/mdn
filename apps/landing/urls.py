65991ff6ecc4c8e912abbe04506452e6c07726db
Fred Wenzel <fwenzel@mozilla.com>
2010-06-28 11:59:27 +0200

    further placeholder pages for addons, apps, docs

--- apps/landing/urls.py
+++ apps/landing/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('landing.views',
     url(r'^$', 'home', name='home'),
+    url(r'^addons/?$', 'addons', name='addons'),
+    url(r'^apps/?$', 'apps', name='apps'),
+    url(r'^docs/?$', 'docs', name='docs'),
     url(r'^mobile/?$', 'mobile', name='mobile'),
     url(r'^web/?$', 'web', name='web'),
 )
--- apps/landing/views.py
+++ apps/landing/views.py
@@ -6,6 +6,21 @@ def home(request):
     return jingo.render(request, 'landing/home.html')
 
 
+def addons(request):
+    """Add-ons landing page."""
+    return jingo.render(request, 'landing/addons.html')
+
+
+def apps(request):
+    """Applications landing page."""
+    return jingo.render(request, 'landing/apps.html')
+
+
+def docs(request):
+    """Docs landing page."""
+    return jingo.render(request, 'landing/docs.html')
+
+
 def mobile(request):
     """Mobile landing page."""
     return jingo.render(request, 'landing/mobile.html')
