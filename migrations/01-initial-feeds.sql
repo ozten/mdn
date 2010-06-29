cf946a4e723da76daa4f71512e55f9a4d30340ee
Fred Wenzel <fwenzel@mozilla.com>
2010-06-29 20:02:28 +0200

    added more bundles and twitter feeds

--- migrations/01-initial-feeds.sql
+++ migrations/01-initial-feeds.sql
@@ -3,6 +3,8 @@ INSERT INTO `feeder_feed` (`id`, `shortname`, `url`, `enabled`, `keep`, `created
 (2, 'tw-mozhacks', 'http://twitter.com/statuses/user_timeline/45496942.rss', 1, 50, NOW(), NOW()),
 (3, 'tw-mozillaweb', 'http://twitter.com/statuses/user_timeline/38209403.rss', 1, 50, NOW(), NOW()),
 (4, 'tw-mozmobile', 'http://twitter.com/statuses/user_timeline/67033966.rss', 1, 50, NOW(), NOW()),
-(5, 'tw-mozillaqa', 'http://twitter.com/statuses/user_timeline/24752152.rss', 1, 50, NOW(), NOW())
-(6, 'planet-mobile', 'http://planet.firefox.com/mobile/rss20.xml', 1, 50, NOW(), NOW())
+(5, 'tw-mozillaqa', 'http://twitter.com/statuses/user_timeline/24752152.rss', 1, 50, NOW(), NOW()),
+(6, 'planet-mobile', 'http://planet.firefox.com/mobile/rss20.xml', 1, 50, NOW(), NOW()),
+(7, 'tw-mozamo', 'http://twitter.com/statuses/user_timeline/15383463.rss', 1, 50, NOW(), NOW()),
+(8, 'tw-planetmozilla', 'http://twitter.com/statuses/user_timeline/39292665.rss', 1, 50, NOW(), NOW())
 ;
