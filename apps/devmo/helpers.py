e95dfc9d61825ba0a141b38a60bcdd34574ffe26
Fred Wenzel <fwenzel@mozilla.com>
2010-07-22 17:11:27 +0200

    new version of urlparams fixes ugly unicode problems

--- apps/devmo/helpers.py
+++ apps/devmo/helpers.py
@@ -1,5 +1,6 @@
 import cgi
 import datetime
+import urllib
 import urlparse
 
 from django.conf import settings
@@ -60,21 +61,24 @@ Add a fragment and/or query paramaters to a URL.
 New query params will be appended to exising parameters, except duplicate
 names, which will be replaced.
 """
-    url_ = urlparse.urlparse(url_)
-    fragment = hash if hash is not None else url_.fragment
+    url = urlparse.urlparse(url_)
+    fragment = hash if hash is not None else url.fragment
 
-    items = []
-    if url_.query:
-        for k, v in cgi.parse_qsl(url_.query):
-            items.append((k, v))
-    for k, v in query.items():
-        items.append((k, v))
+    # Use dict(parse_qsl) so we don't get lists of values.
+    q = url.query
+    query_dict = dict(urlparse.parse_qsl(smart_str(q))) if q else {}
+    query_dict.update((k, v) for k, v in query.items())
 
-    items = [(k, unicode(v).encode('raw_unicode_escape')) for
-             k, v in items if v is not None]
+    query_string = _urlencode([(k, v) for k, v in query_dict.items()
+                               if v is not None])
+    new = urlparse.ParseResult(url.scheme, url.netloc, url.path, url.params,
+                               query_string, fragment)
+    return new.geturl()
 
-    query_string = urlencode(items)
 
-    new = urlparse.ParseResult(url_.scheme, url_.netloc, url_.path,
-                               url_.params, query_string, fragment)
-    return jinja2.Markup(new.geturl())
+def _urlencode(items):
+    """A Unicode-safe URLencoder."""
+    try:
+        return urllib.urlencode(items)
+    except UnicodeEncodeError:
+        return urllib.urlencode([(k, smart_str(v)) for k, v in items])
